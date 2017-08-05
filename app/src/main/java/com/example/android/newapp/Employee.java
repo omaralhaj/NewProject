30c8c06d698b5fa3a15126bba1d3075fe58614c6
omaralhaj <iphony@windowslive.com>
2017-08-05 16:37:24 +0300

    .

---
+++ app/src/main/java/com/example/android/newapp/Employee.java
@@ -0,0 +1,35 @@
+package com.example.android.newapp;
+
+/**
+ * Created by alhajery on 7/29/17.
+ */
+
+public class Employee {
+
+    private String name ;
+
+    private String age ;
+
+    public Employee(String name , String age) {
+        this.name = name;
+        this.age = age ;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+
+        this.name = name ;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+}
